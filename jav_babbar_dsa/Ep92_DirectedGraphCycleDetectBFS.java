69a3a4878ca0059402c7bb7522c99cfb971d64e7
badrinath <115296343+badrinath-expo@users.noreply.github.com>
2025-06-21 19:41:17 +0000

    upto 92

--- jav_babbar_dsa/Ep92_DirectedGraphCycleDetectBFS.java
+++ jav_babbar_dsa/Ep92_DirectedGraphCycleDetectBFS.java
@@ -1,48 +1,44 @@
-public class Ep91_TopSortKhansAlgo {
-    // Find indegree of all nodes
-    // queue - 0 indegree nodes insert
-       public static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> edges, int v, int e) {
+public class Ep92_DirectedGraphCycleDetectBFS {
+//DAG - gets valid topological sort
+//cyclic grah - invalid topological sort
 
+//Find in chatgpt why topological sort works, why
+ public static boolean detectCycleInDirectedGraph(int n, ArrayList<ArrayList<Integer>> edges) {
         // Write your code here.
         Map<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>();
-
         for (int i = 0; i < e; i++) {
-            int a = edges.get(i).get(0);
-            int b = edges.get(i).get(1);
+            int a = edges.get(i).get(0) - 1;
+            int b = edges.get(i).get(1) - 1;
 
             // Initialize adjacency lists if not present
             adj.putIfAbsent(a, new ArrayList<Integer>());
             adj.putIfAbsent(b, new ArrayList<Integer>());
-
             adj.get(a).add(b);
         }
 
-        boolean visited[] = new boolean[v + 1];
-
-        int indegree[] = new int[v + 1];
+        int indegree[] = new int[v +1];
 
         for (Integer key : adj.keySet()) {
-
             for (Integer i : adj.getOrDefault(key, new ArrayList<>())) {
                 indegree[i]++;
             }
         }
 
         Queue<Integer> q = new LinkedList<>();
-
         for (int i = 0; i < v; i++) {
             if (indegree[i] == 0) {
                 q.add(i);
             }
         }
 
-        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int cnt = 0;
 
         while (!q.isEmpty()) {
 
             int front = q.peek();
             q.remove();
-            ans.add(front);
+            cnt++;
+
             for (Integer child : adj.getOrDefault(front, new ArrayList<>())) {
                 indegree[child]--;
                 if (indegree[child] == 0) {
@@ -50,13 +46,9 @@ public class Ep91_TopSortKhansAlgo {
                 }
             }
         }
-        return ans;
+        return cnt !=n; //cnt can only be equal acyclic graph, find why count
     } //T.C - O(N+E)  S.C - O(N+E)
-
-    public static void main(String[] args) {
-        /*
-         * 
-         * Understand the concept from chatgpt
-         */
+public static void main(String[] args) {
+        
     }
 }
